f41597b31456de404fa3c28cd57f9538730d37f4
Fidan <fidannabd81_gmail,com>
2026-05-16 08:59:44 +0400

    task

--- 2script.js
+++
@@ -1,8 +0,0 @@
-let mehsulAdi="saat";
-let mehsulQiymeti=400;
-let endirimFaizi=20;
-let stokdaVarmi=true;
-let endirimliQiymet=mehsulQiymeti-(mehsulQiymeti*endirimFaizi/100);
-console.log("Mehsul:" + mehsulAdi);
-console.log("Endirimli Qiymet:" + endirimliQiymet +"AZN" );
-console.log(mehsulQiymeti > 50 && stokdaVarmi);
